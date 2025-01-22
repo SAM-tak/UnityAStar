1338bf3565a49ea1ce90301ef21bed4cfb187780
Osamu Takasugi <osamu.takasugi@gmail.com>
2025-01-23 07:49:30 +0900

    Update Vector3Node.cs

--- Runtime/Vector3Node.cs
+++ Runtime/Vector3Node.cs
@@ -6,7 +6,7 @@ namespace SAMtak.AStar
 
     public abstract class Vector3Node : FloatCostNode
     {
-        public Vector3Int position;
+        public Vector3 position;
 
         public override float EstimateCostTo(INode other) => Vector3.Distance(position, ((Vector3Node)other).position);
     }
